e2432fad00bf4036a5cec17df72c53fadd15b9ff
zhushh <zsh5318@gmail.com>
2014-12-31 19:15:32 +0800

    Initail commit

---
+++ sayHello.c
@@ -0,0 +1,14 @@
+/***********************************************************************
+ *  > File Name: sayHello.c
+ *  > Author: zhushuhuang
+ *  > Mail: zsh5318@gmail.com
+ *  > Created Time: Wed 31 Dec 2014 07:13:11 PM CST
+***********************************************************************/
+
+#include <stdio.h>
+
+int main() {
+    printf("Hello!\n");
+    return 0;
+}
+
